8ffdad19c47a792d9c7566b37e95f4dcd8a954fe
admenmod <admenmod@gmail.com>
2024-08-22 14:16:15 +0300

    [v0.2.1] bug fix

--- src/config.ts
+++ src/config.ts
@@ -11,8 +11,12 @@ export const isClient = GLOBAL_ENVIRONMENT === 'browser';
 
 if(GLOBAL_ENVIRONMENT === 'unknown') throw new Error(`GLOBAL_ENVIRONMENT: ${GLOBAL_ENVIRONMENT}`);
 
+console.log(process.env.API_DOMAIN, isServer);
+if(isServer && !process.env.API_DOMAIN) throw new Error('API_DOMAIN is emrty');
+if(isServer && !process.env.HOSTNAME) throw new Error('HOSTNAME is emrty');
 
-export const API_ROOT_SSR = `${process.env.API_DOMAIN || 'https://ranobe.monster:44300'}/api`;
-export const API_ROOT_CSR = `${process.env.HOSTNAME || 'http://localhost:3000'}/api`;
+
+export const API_ROOT_SSR = `${process.env.API_DOMAIN}/api`; // https://ranobe.monster:PORT
+export const API_ROOT_CSR = `${isServer ? process.env.HOSTNAME : location.origin}/api`; // http://localhost:3000
 
 export const API_ROOT = isServer ? API_ROOT_SSR : API_ROOT_CSR;
